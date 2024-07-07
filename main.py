3c721b0947ee80eb0c0c8caf88e900c0c5c74cfc
WalidTorfa <walidtorfa@gmail.com>
2024-07-07 20:55:03 +0400

    testing

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 from prepro import Image_reco
 import torch
-
-
-# Ask the user to input a series of labels, separated by commas
-user_input = input("Enter labels separated by commas: ")
-
-# Split the string into a list at each comma
-labels_list = user_input.split(',')
-
-# Optionally, strip whitespace from each label
-labels_list = [label.strip() for label in labels_list]
+from test_branches import *
 
 
 
 
 
+x = int(input("Do you want the model or the test"))
 
-image_object = Image_reco("train", labels_list)
+if x == 1:
+    # Ask the user to input a series of labels, separated by commas
+    user_input = input("Enter labels separated by commas: ")
 
-print(torch.backends.mps.is_available())
+    # Split the string into a list at each comma
+    labels_list = user_input.split(',')
 
-dataframe = image_object.getting_data()
-train_gen,val_gen, test_gen = image_object.preprocessing_data(dataframe)
-model = image_object.build_model()
-x = image_object.train_model(model,train_gen,val_gen,test_gen)
+    # Optionally, strip whitespace from each label
+    labels_list = [label.strip() for label in labels_list]
 
+    image_object = Image_reco("train", labels_list)
+
+    print(torch.backends.mps.is_available())
+
+    dataframe = image_object.getting_data()
+    train_gen,val_gen, test_gen = image_object.preprocessing_data(dataframe)
+    model = image_object.build_model()
+    x = image_object.train_model(model,train_gen,val_gen,test_gen)
+
+elif x == 2:
+    print(f"Person name is {get_person_name()} and Laptop owner is {get_laptop_owner()}")
+
+else:
+    print("Undefined choice")
---
+++ test_branches.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+import numpy as np
+
+
+def get_person_name():
+    return "Omar"
+
+def get_laptop_owner():
+    return "walid"
